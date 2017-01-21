7b9f33d9c1b7377e8234b861088d741187274922
Daniel Kutyła <danielkutyla@wp.pl>
2017-01-21 23:03:29 +0100

    clock added

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,20 @@
+$('#clock').countdown('2017/10/28 16:00:00')
+    .on('update.countdown', function(event) {
+        var format = '%H:%M:%S';
+
+        if(event.offset.totalDays > 1 || event.offset.totalDays == 0) {
+            format = '%-d dni ' + format;
+        } else {
+            format = '%-d dzień ' + format;
+        }
+
+        if(event.offset.weeks > 5 || event.offset.weeks == 0) {
+            format = '%-w tygodni ' + format;
+        } else if (event.offset.weeks < 5 && event.offset.weeks < 1) {
+            format = '%-w tygodnie ' + format;
+        } else {
+            format = '%-w tydzień ' + format;
+        }
+
+        $(this).html(event.strftime(format));
+    });
