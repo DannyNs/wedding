7337be6d0470e5ec2925002c37a4ace0f81ac482
Daniel Kutyła <danielkutyla@wp.pl>
2017-01-26 17:09:01 +0100

    kasia i daniel photo resize og params

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -3,17 +3,9 @@ $('#clock').countdown('2017/10/28 16:00:00')
         var format = '<p>%H:%M:%S</p>';
 
         if (event.offset.totalDays > 1 || event.offset.totalDays == 0) {
-            format = '%-d dni ' + format;
+            format = '%-D dni ' + format;
         } else {
-            format = '%-d dzień ' + format;
-        }
-
-        if (event.offset.weeks > 5 || event.offset.weeks == 0) {
-            format = '%-w tygodni ' + format;
-        } else if (event.offset.weeks < 5 && event.offset.weeks < 1) {
-            format = '%-w tygodnie ' + format;
-        } else {
-            format = '%-w tydzień ' + format;
+            format = '%-D dzień ' + format;
         }
 
         $(this).html(event.strftime(format));
